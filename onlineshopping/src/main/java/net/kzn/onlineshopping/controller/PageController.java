326b575d9eeec75ed789d69e3251cf70f3ca8a56
hunt100 <anvar.hunt@gmail.com>
2017-11-10 06:03:06 +0600

    Using Bootstrap and view representation

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -1,6 +1,7 @@
 package net.kzn.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,5 +14,28 @@ public class PageController {
 		mv.addObject("greeting","Welcome to Spring Web MVC");
 		return mv;
 	}
+	
+//	@RequestMapping(value="/test")
+//	//public ModelAndView test(@RequestParam("greeting")String greeting) {
+//	public ModelAndView test(@RequestParam(value="greeting",required=false)String greeting) {
+//		if (greeting == null) {
+//			greeting = "Hello Hello my little friend";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting",greeting);
+//		return mv;
+//	}
+	
+	@RequestMapping(value="/test/{greeting}") // greeting is dynamic
+	//public ModelAndView test(@RequestParam("greeting")String greeting) {
+	public ModelAndView test(@PathVariable(value="greeting")String greeting) {
+		if (greeting == null) {
+			greeting = "Hello Hello my little friend";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
+	}
+	
 	
 }
